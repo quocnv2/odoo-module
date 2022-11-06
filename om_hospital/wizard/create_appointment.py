2ecffb7d51a1f25ef8d4a4c3eddbb88c140dbc8f
Nguyen Van Quoc <quoc.nguyenvan2000@gmail.com>
2022-11-07 04:17:30 +0700

    return view odoo

--- om_hospital/wizard/create_appointment.py
+++ om_hospital/wizard/create_appointment.py
@@ -6,7 +6,7 @@ class HospitalAppointment(models.TransientModel):
     _description = 'Create Appointment Wizard'
     name = fields.Char(string='Name')
     patient_id = fields.Many2one('hospital.patient', string="Patient", required=True)
-    date_appointment = fields.Date(string="Date", required=True)
+    date_appointment = fields.Date(string="Date")
 
     def action_create_appointment(self):
         print('Clicked')
@@ -14,4 +14,18 @@ class HospitalAppointment(models.TransientModel):
             'patient_id': self.patient_id.id,
             'date_appointment': self.date_appointment
         }
-        self.env['hospital.appointment'].create(vals)
+        # patient_id = self.env['hospital.appointment'].create(vals)
+        return {
+            'name': _('Appointment'),
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'hospital.appointment',
+            'res_id': self.env['hospital.appointment'].create(vals).id,
+            'target': 'new',
+        }
+
+    def action_view_appointment(self):
+        action = self.env.ref('om_hospital.appointment_actions').read()[0]
+        action['domain'] = [('patient_id', '=', self.patient_id.id)]
+        return action
